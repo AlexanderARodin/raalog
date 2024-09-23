406468559068b5a8fcbd4d05f999d40a4acb52be
raa <alexander.thequeen@gmail.com>
2024-09-23 21:20:40 +0300

    hotfix test

--- tests/simple.rs
+++ tests/simple.rs
@@ -7,20 +7,12 @@ fn simple_stdout_stderr() -> anyhow::Result<()> {
 
     l.set_stderr_mode();
     log::info!("mini info");
-
-    l.set_silent_mode();
-    log::error!("mini info");
-
-    Ok(())
-}
-#[test]
-fn mutliline_stderr() -> anyhow::Result<()> {
-    let l = raalog::init()?;
-    l.set_stderr_mode();
-
     log::info!("");
     log::info!("mini\ninfo");
     log::error!("mini\ninfo\nrmation!");
+
+    l.set_silent_mode();
+    log::error!("mini info");
 
     Ok(())
 }
